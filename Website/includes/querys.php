1962d783040688883f6f6aed6cefc8ad209f53fe
Mathijs Haakman <haakmam002@hva.nl>
2016-01-28 17:50:56 +0100

    Updated queries for new database model

--- Website/includes/querys.php
+++ Website/includes/querys.php
@@ -1,30 +1,110 @@
 <?php
 include_once 'db_connect.php';
 
-function getValueById($id){
+function getPWM($colour, $userId, $lightNumber)
+{
     global $mysqli;
-    if($mysqli->connect_error){
+    if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
-    $sql = "SELECT value FROM LightSettings WHERE id=\"$id\"";
+    $sql = "SELECT \"$colour\" FROM Settings_per_light WHERE UserID = \"$userId\" AND LightNumber = \"$lightNumber\"";
     $result = $mysqli->query($sql);
-    if($result->num_rows > 0){
-        while($row = $result->fetch_assoc()){
-            $SQLResult =  $row["value"];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $SQLResult = $row["$colour"];
         }
     } else {
-        echo "0 results";
+        echo "No results for getValueById\n";
     }
     return $SQLResult;
 }
 
-function updateValueById($id, $value){
+function updateGPIOPin($id, $lightNr, $PinRed, $PinGreen, $PinBlue)
+{
+    global $mysqli;
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    $sql = "INSERT INTO Settings_per_light (UserID, LightNumber, GPIOPinRed, GPIOPinGreen, GPIOPinBlue) VALUES
+            (\"$id\", \"$lightNr\", \"$PinRed\", \"$PinGreen\", \"$PinBlue\")
+            ON DUPLICATE KEY
+            UPDATE GPIOPinRed = \"$PinRed\", GPIOPinGreen = \"$PinGreen\", GPIOPinBlue = \"$PinBlue\"
+            ";
+    if ($mysqli->query($sql) === TRUE) {
+    } else {
+    }
+}
+
+function getMaxLampsById($id)
+{
+    global $mysqli;
+    $SQLResult = 0;
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    $sql = "SELECT maxNumberOfLights FROM Lights_per_user WHERE UserID = \"$id\"";
+    $result = $mysqli->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $SQLResult = $row['maxNumberOfLights'];
+        }
+    } else {
+        return 0;
+    }
+    return $SQLResult;
+}
+
+function setMaxLamps($maxLamps, $id)
+{
+    global $mysqli;
+    if ($mysqli->connect_error) {
+        die("Connection Failed: " . $mysqli->connect_error);
+    }
+    $sql = "INSERT INTO Lights_per_user (UserID, maxNumberOfLights) VALUES (\"$id\", \"$maxLamps\")
+            ON DUPLICATE KEY
+            UPDATE maxNumberOfLights = \"$maxLamps\"
+            ";
+    if ($mysqli->query($sql) === TRUE) {
+    } else {
+    }
+}
+
+function getSelected($colour, $lampNr, $id){
+    global $mysqli;
+    $SQLResult = null;
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    $sql = "SELECT $colour FROM Settings_per_light WHERE UserID = \"$id\" and LightNumber = \"$lampNr\"";
+    $result = $mysqli->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $SQLResult = $row["$colour"];
+        }
+    } else {
+        return null;
+    }
+    return $SQLResult;
+}
+
+function deleteUnnecessaryRows($userID, $maxLamps){
     global $mysqli;
     if($mysqli->connect_error){
         die("Connection failed: " . $mysqli->connect_error);
     }
-    $sql = "UPDATE LightSettings SET value = \"$value\" WHERE id=\"$id\"";
-    if ($mysqli->query($sql) === TRUE) {
+    $sql = " SELECT COUNT(LightNumber) AS maxLights FROM Settings_per_light WHERE UserID = \"$userID\"";
+    $result = $mysqli->query($sql);
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            $SQLResult = $row["maxLights"];
+        }
     } else {
+        return null;
+    }
+    if($SQLResult > $maxLamps){
+        for($SQLResult; $SQLResult > $maxLamps; $SQLResult--){
+            $sql = "DELETE FROM Settings_per_light WHERE UserID = \"$userID\" AND LightNumber = \"$SQLResult\"";
+            $mysqli->query($sql);
+        }
     }
 }
